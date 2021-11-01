5bacbd0422bf42b56e2494cec15b236653e3f5f0
reshad020 <aehtesham2002@gmail.com>
2021-11-01 22:22:59 +0600

    ok

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const port = process.env.PORT || 4000;
 const cors = require('cors');
 const app = express();
+const objectId = require('mongodb').ObjectId;
 
 require('dotenv').config();
 
@@ -17,11 +18,34 @@ async function run(){
         await client.connect();
         const database = client.db('soloTraveler');
         const serviceCollection = database.collection('services');
+        const bookingCollection = database.collection('bookings');
 
-        // app.get('/services', async (req, res)=>{
-        //     console.log("get hitted");
-        //     res.send("lsjfsjdf");
-        // })
+        app.get('/services', async(req,res) =>{
+            const cursor = serviceCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+
+        })
+        app.get('/services/:id', async(req,res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.json(service)
+
+        })
+        app.get('/addbooking/:id', async(req,res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const booking = await bookingCollection.findOne(query);
+            res.json(booking);
+
+        })
+        app.get('/addbooking', async(req,res) =>{
+            const cursor = bookingCollection.find({});
+            const bookings = await cursor.toArray();
+            res.send(bookings);
+
+        })
 
         app.post('/services', async(req,res) =>{
                 const service = req.body;
@@ -30,6 +54,22 @@ async function run(){
                 console.log(result);
                 res.json(result);
                 console.log('hit api')
+        })
+        app.post('/addbooking', async(req,res) =>{
+                const booking = req.body;
+
+                const result = await bookingCollection.insertOne(booking);
+                console.log(result);
+                res.json(result);
+                console.log('hit api')
+        })
+
+        //Delete api
+        app.delete('/addbooking/:id', async(req,res) =>{
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const result = await bookingCollection.deleteOne(query)
+            res.json(result);
         })
     }
     finally{
